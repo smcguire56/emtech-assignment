494e5b9a1b5e4373df6d76f629a00e33dd831a07
smcguire56 <sean.mcguire56@gmail.com>
2018-11-27 21:37:25 +0000

    added word doc for numpy

--- 4. Digit recognition script/digitrec.py
+++ 4. Digit recognition script/digitrec.py
@@ -5,6 +5,9 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.utils import np_utils
+from PIL import Image
+import numpy as np
+
 
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -18,6 +21,7 @@ X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1).as
 # normalize inputs from 0-255 to 0-1
 X_train/=255
 X_test/=255
+# https://hackernoon.com/what-is-one-hot-encoding-why-and-when-do-you-have-to-use-it-e3c6186d008f
 
 # one hot encoding for labels, the only output is a number between 1 and 10 
 # eg 5 : [0,0,0,0,0,1,0,0,0,0]
@@ -58,3 +62,12 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, bat
 metrics = model.evaluate(X_test, y_test, verbose=0)
 
 print(metrics)
+
+for index in range(10):
+    img = Image.open('data/' + str(index) + '.png').convert("L")
+    img = img.resize((28,28))
+    im2arr = np.array(img)
+    im2arr = im2arr.reshape(1,28,28,1)
+    # Predicting the Test set results
+    y_pred = model.predict(im2arr)
+    print(y_pred)
